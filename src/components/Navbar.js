2e70df6b70add5d70033d13a8c9984cfa82f20b1
MMzzy <majmezic@gmail.com>
2026-01-15 02:09:18 +0100

    uređeni navbar i dodan bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,27 @@
+import React from "react"
+import {MdHome} from 'react-icons/md';
+import './Navbar.css';
+
 const Navbar = () => {
     return (
         <>
             <nav className="navbar navbar-extend-lg">
-                <div className="container">
-
-                </div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#home">
+                            <MdHome/> HOME
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#about">ABOUT</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#contact">CONTACT</a>
+                    </li>
+                </ul>
             </nav>
         </>
     )
 }
+
+export default Navbar;
